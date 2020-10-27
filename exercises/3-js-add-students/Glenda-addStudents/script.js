263b4bd45959f920450e715cd93dc7880b554715
Glenda <glenda.rimondi@gmail.com>
2020-10-27 12:05:13 +0100

    Glenda-get form values
    selezionato campi input nella form.
    creato nuovo oggetto "studenti" con caricati i valuri degli input della form.

--- exercises/3-js-add-students/Glenda-addStudents/script.js
+++ exercises/3-js-add-students/Glenda-addStudents/script.js
@@ -1,6 +1,13 @@
 function addNewStudent() {
-  console.log("Aggiungi Studente");
+  var inputs = document.getElementsByTagName("input")
+  var student = {
+      name: inputs.name.value, 
+      surname: inputs.surname.value,
+      age: inputs.age.value
+  }
   /* fino a qui funziona ma la pagina si ricarica subito quando clicco sul pulsante*/
+  console.log(student);
+
   var form = document.getElementById("newStudentForm");
   /*con var form gli dico di non comportarsi come di default in modo che non refreshi */
   /*ho riempito la variabile form con l'id della form che ho in html */
@@ -12,3 +19,6 @@ function addNewStudent() {
 /* abbiamo personalizzato il comportamento di default di una form. Ora quando apro in browser 
 e clicco sul punsante sulla console mi stampa la scritta Aggiungi Studente senza che si cancelli subito dopo.
 La stampa della scritta mi serve solo per verificare che il ciclo della funzione giri correttamente */
+
+/*sarebbe anche interessante stampare nella console quello che viene inserito nei vari campi
+quindi alla riga 2 del file js vado a modificare il contenuto delle parentesi sostituendo "Aggiungi studente" */
