55f72afe858db14d9fc870eec97d70d856a149dc
mircocotugno <mirco.cotugno@gmail.com>
2020-10-28 11:39:01 +0100

    Update script.js
    creato l'array di studenti, aggiunta la validazione della form, aggiunta la classe che mostra un errore nella form, definita la funzione per aggiungere uno studente alla lista visibile nella pagina

--- exercises/mirco-add-student/script.js
+++ exercises/mirco-add-student/script.js
@@ -1,15 +1,42 @@
+var students = [
+  {
+    name: "Alessandra",
+    surname: "Pizzol",
+    age: 21,
+  },
+];
+
 function addNewStudent() {
   var inputs = document.getElementsByTagName("input");
+  var form = document.getElementById("newStudentForm");
   var student = {
     name: inputs.name.value,
     surname: inputs.surname.value,
     age: inputs.age.value,
-  }
-  console.log(student);
+  };
 
-  var idForm = "newStudentForm";
-  var form = document.getElementById(idForm);
+  if (student.name && student.surname && student.age) {
+    console.log("ok");
+    students.push(student);
+    form.classList.remove("has-error");
+    addStudentToList(student);
+    form.reset();
+  } else {
+    console.log("err");
+    form.classList.add("has-error");
+  }
+  console.log(students);
+
   form.addEventListener("click", function (event) {
     event.preventDefault();
   });
+}
+
+function addStudentToList(student) {
+  var list = document.getElementById("studentList");
+  var studentItem = document.createElement("li");
+  studentItem.appendChild(
+    document.createTextNode(student.name + " " + student.surname + " - " + student.age)
+  );
+  list.appendChild(studentItem);
 }
