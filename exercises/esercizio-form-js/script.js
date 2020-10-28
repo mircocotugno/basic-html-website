02b91baf8a76af083eb102fa2bc34706db879d0b
mircocotugno <mirco.cotugno@gmail.com>
2020-10-28 12:26:18 +0100

    Fix conflicts
    Attenzione a selezionare la branch corretta quando fai delle modifiche al codice

--- exercises/esercizio-form-js/script.js
+++ exercises/esercizio-form-js/script.js
@@ -1,6 +1,5 @@
 function addBand() {
     var form = document.getElementById("bookingForm");
-<<<<<<< Updated upstream
     /* console.log(form); */
     form.addEventListener("click", function (event) {
         event.preventDefault();
@@ -16,21 +15,4 @@ function addBand() {
        email: inputs.email.value,
    }
    console.log(band); //tutto ok 
-=======
-    /* console.log(form); */ //FINO A QUI TUTTO OK
-
-    var inputs = document.getElementsByTagName("inputs");
-    /* console.log(inputs); */ //esce un HTML collections [] quindi ok, credo
-
-    form.addEventListener("click", function (event) {
-        event.preventDefault();
-      }); //anche questa OK
-    
-    var band = {
-      name: inputs.name.value,
-      members: inputs.members.value,
-      email: inputs.email.value,
-      };
-    console.log(band); // qui mi dà SYNTAX ERROR
->>>>>>> Stashed changes
 }
