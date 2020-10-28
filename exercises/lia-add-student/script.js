8a0fea8ecb67573b5578af7add59f41a7aa6de50
Lia Maccapani <73106505+liamaccapani@users.noreply.github.com>
2020-10-28 11:39:11 +0100

    Update script.js
    Creato un array di studenti,
    Aggiunta la validazione della form,
    Aggiunta la classe che mostra un errore nella form,
    Definita la funzione per aggiungere uno studente alla lista visibile nella pagina

--- exercises/lia-add-student/script.js
+++ exercises/lia-add-student/script.js
@@ -9,20 +9,47 @@
         event.preventDefault();
     }) */
 
+var students = [
+    {
+      name: "Lia",
+      surname: "Maccapani",
+      age: 21
+    }
+];
+
 function addNewStudent() {
     var inputs = document.getElementsByTagName("input");
+    var idForm = "newStudentForm";
+    var form = document.getElementById(idForm);
     var student = { //oggetto in cui posso instanziare diverse chiavi/valori
      name: inputs.name.value, //name: è la chiave, a fianco c'è il valore
      surname: inputs.surname.value, 
      age: inputs.age.value,
+    };
+
+    if (student.name && student.surname && student.age) {
+        console.log("ok");
+        students.push(student);
+        form.classList.remove("has-error");
+        addStudentToList(student);
+        form.reset();
+    } else {
+        console.log("err");
+        form.classList.add("has-error");
     }
-    console.log(student);
-    
-    var idForm = "newStudentForm";
-    var form = document.getElementById(idForm);
+    console.log(students);
+
     form.addEventListener("click", function (event) {
-    event.preventDefault();
+        event.preventDefault(); 
  });
 }
 
+function addStudentToList(student) {
+    var list = document.getElementById("studentList");
+    var studentItem = document.createElement("li");
+    studentItem.appendChild(
+        document.createTextNode(student.name + " " + student.surname + " - " + student.age)
+    );
+    list.appendChild(studentItem);
+}
  /* debugger;*/
