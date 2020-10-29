bf3dd4c7092843c50c7593780224cd1301ddb1be
mircocotugno <mirco.cotugno@gmail.com>
2020-10-29 09:40:36 +0100

    Project init

---
+++ exercises/4-htmlCssJs-price-quote/script.js
@@ -0,0 +1,21 @@
+// Al caricamento della pagina stampo i preventivi esistenti
+
+
+
+// Estraggo le informazioni dalla form
+ 
+  // Verifico la correttezza dei campi
+
+  // Compongo i dati del preventivo
+
+  // Eseguo i calcoli per ottenerere il prezzo totale
+
+  // Da capire anteprima costo preventivo
+
+  // Inserico il nuovo preventivo nella lista (array)
+
+
+
+// stampo il nuovo elemento nella lista
+
+  // Creo l'elemento della lista
